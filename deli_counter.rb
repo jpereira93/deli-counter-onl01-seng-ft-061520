e8831f2585b85ce0d79d81eea01a44c3e42e8c3e
Jessica Pereira <jcpereira070@gmail.com>
2020-06-22 21:10:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def line(people)
   if people.length >= 1
     katz_deli = []
@@ -7,4 +8,32 @@ puts "The line is currently empty"
 end
 
 
+=======
+katz_deli = []
+
+def line (array)
+   if array.count == 0
+      puts "The line is currently empty."
+   else
+      response = "The line is currently:"
+      array.each_with_index do |name, index|
+         response += " #{index+1}. #{name}" 
+   end
+   puts response
+   end
+end
+
+def take_a_number (array, name)
+   array.push(name)
+   puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+   if array.count == 0
+      puts "There is nobody waiting to be served!"
+   else
+      puts "Currently serving #{array.shift}."
+   end
+end
+>>>>>>> 580ad62fa0c99af26c8ee02c80f8aa3d3669564c
 
